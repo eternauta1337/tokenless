c88982e026284982eef58d6c44583e19cc2cf110
Alejandro Santander <palebluedot@gmail.com>
2017-08-07 12:27:44 -0300

    Allow async/await in thunk actions

--- config/polyfills.js
+++ config/polyfills.js
@@ -12,3 +12,6 @@ require('whatwg-fetch');
 // Object.assign() is commonly used with React.
 // It will use the native implementation if it's present and isn't buggy.
 Object.assign = require('object-assign');
+
+require('babel-register');
+require('babel-polyfill');
