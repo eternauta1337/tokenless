77d673bc7c5d20928f71e17475fa45506873ec61
Alejandro Santander <palebluedot@gmail.com>
2017-08-06 23:32:03 -0300

    Minor cleanup

--- src/components/App.js
+++ src/components/App.js
@@ -5,13 +5,11 @@ class App extends Component {
   render() {
     return (
       <div>
-
         <nav className="navbar pure-menu pure-menu-horizontal">
           <Link to="/">Tokenless</Link>
           <Link to="/">Browse Markets</Link>
           <Link to="/">Create a Market</Link>
         </nav>
-
         {this.props.children}
       </div>
     );
