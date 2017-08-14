7e88d5881ab30c8cdf494a6d29a5ebcb798e77c8
Alejandro Santander <palebluedot@gmail.com>
2017-08-14 20:53:54 -0300

    Fix bug in place bet action caused by rename refactor

--- src/prediction/actions/PlaceBetAction.js
+++ src/prediction/actions/PlaceBetAction.js
@@ -1,16 +1,16 @@
 import { connectPrediction } from '.';
 import { forgetPreview } from '../../market/actions/ForgetPredictionPreviewAction';
 
-export function placeBet(prediction, betEther) {
-  console.log('placeBet()', prediction, betEther);
+export function placeBet(bet, betEther) {
+  console.log('placeBet()', bet, betEther);
   return async function(dispatch, getState) {
 
     const web3 = getState().network.web3;
     const prediction = getState().prediction.contract;
 
     // Listen for bet event...
-    prediction.BetEvent().watch((error, result) => {
-      console.log('BetEvent', error, result);
+    prediction.BetEvent().watch((error) => {
+      console.log('BetEvent');
       if(error) {
         console.log('placing bet failed');
       }
@@ -24,7 +24,7 @@ export function placeBet(prediction, betEther) {
     // Place bet
     const betWei = web3.toWei(betEther, 'ether');
     console.log('placing bet: ', prediction, betWei, getState().network.activeAccountAddress);
-    await prediction.bet(prediction, {
+    await prediction.bet(bet, {
       from: getState().network.activeAccountAddress,
       value: betWei
     });
