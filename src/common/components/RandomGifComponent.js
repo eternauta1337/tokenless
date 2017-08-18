2df8e331e44404b1bae38d4a29d7e6ea29127f96
Alejandro Santander <palebluedot@gmail.com>
2017-08-18 10:32:45 -0300

    Bugfix on gifs

--- src/common/components/RandomGifComponent.js
+++ src/common/components/RandomGifComponent.js
@@ -7,7 +7,7 @@ class RandomGifComponent extends React.Component {
     const q = "computers"; // search query
 
     const request = new XMLHttpRequest();
-    request.open('GET', 'http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag='+q, true);
+    request.open('GET', 'https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag='+q, true);
 
     request.onload = function() {
       if (request.status >= 200 && request.status < 400){
